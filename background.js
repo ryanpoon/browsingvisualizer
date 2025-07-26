b9978e59777209f7c173e93ca5c17f4bd6a57699
ryanpoon <ryanpoon2004@gmail.com>
2025-07-26 13:34:38 -0400

    added icon dot to indicate whether tracking is active

--- background.js
+++ background.js
@@ -25,6 +25,35 @@ chrome.runtime.onMessage.addListener((message, sender) => {
     }
 });
 
+// Listen for changes in the active state to update the icon
+const stateToIcon = {
+    "active": "icons/track_active.png",
+    "inactive": "icons/track_inactive.png",
+};
+
+// update the icon based on state
+function updateIcon(active) {
+    if (active) {
+        iconPath = stateToIcon.active;
+    } else {
+        iconPath = stateToIcon.inactive;
+    }
+    chrome.action.setIcon({ path: iconPath });
+}
+
+// on extension load, read state from storage
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.local.get("active", (data) => {
+        updateIcon(data.active);
+    });
+});
+
+// listen for changes in storage to update the icon
+chrome.storage.onChanged.addListener((changes, areaName) => {
+    if (areaName === "local" && changes.active) {
+        updateIcon(changes.active.newValue);
+    }
+});
 
 // potential todo: track tab movement, for now just worry about page loads
 // chrome.tabs.onActivated.addListener(function(activeInfo) {
